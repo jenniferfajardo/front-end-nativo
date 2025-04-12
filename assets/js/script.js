c4896e08ee512933d7e45f3b69e92580d344aff5
jfajardo83 <jafajardo83@misena.edu.co>
2025-04-11 20:20:18 -0500

    Se completan funciones de la CRUD para actualizar usuario y cambiar estado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 //1. Definir el API
 const API_BASE = "http://127.0.0.1:8000"
 
+let userUpdate=null;
+
 //2. Listar datos desde el API
 async function loadUsers(){
 
@@ -24,8 +26,8 @@ async function loadUsers(){
     <span class="badge ${badgeClass}"> ${badgeText} </span>
     </td>
     <td>
-    <i class="fa-solid fa-pencil" title="Editar"></i>
-    <i class="fa-solid fa-toggle-on" title="Cambiar de estado"></i>
+    <i class="fa-solid fa-pencil" title="Editar" onclick=openEditModal(${u.id})></i>
+    <i class="fa-solid fa-toggle-on" title="Cambiar de estado" onclick=changeStatus(${u.id},${u.estado})></i>
     </td>
     </tr>`;
 
@@ -36,15 +38,48 @@ async function loadUsers(){
 //Gestión del modal
 function openModal(){
   document.getElementById("modal").style.display= "flex";
-  document.getElementById("userForm").reset();
+  if(userUpdate){
+    document.getElementById("formTitle").textContent="Actualizar Usuario";
+    document.getElementById("submitButton").textContent="Actualizar";
+  }
+  else{
+    document.getElementById("formTitle").textContent="Crear Usuario";
+    document.getElementById("submitButton").textContent="Crear";
+    document.getElementById("userForm").reset();
+  }
+  
 }
 
 function closeModal(){
   document.getElementById("modal").style.display= "none";
+  userUpdate= null;
 }
 
+async function openEditModal(id){
+  const res= await fetch(`${API_BASE}/user/get-user/${id}`);
+  const response= await res.json();
 
-//2. Create 
+  if(!res.ok || !response.data){
+    return Swal.fire(
+      "Error",
+      "No se pudo cargar el usuario",
+      "error"
+    )
+  }
+
+  const user=response.data;
+
+  document.getElementById("nombre").value = user.nombre || "";
+  document.getElementById("apellido").value = user.apellido || "";
+  document.getElementById("correo").value = user.correo || "";
+  document.getElementById("numtelefono").value = user.numtelefono || "";
+  document.getElementById("password").value = user.password || "";
+
+  userUpdate= id;
+  openModal();
+}
+
+//2. Create / Update
 async function createUser() {
 
   const user={
@@ -55,8 +90,11 @@ async function createUser() {
       password: document.getElementById("password").value,
   }
 
-  const endpoint=`${API_BASE}/user/create-user`;
-  const method ="POST";
+  const endpoint= userUpdate
+  ? `${API_BASE}/user/update-user/${userUpdate}` 
+  : `${API_BASE}/user/create-user`;
+  
+  const method = userUpdate ? "PUT" : "POST";
 
   const res= await fetch(endpoint, {
     method,
@@ -69,7 +107,7 @@ async function createUser() {
   if(res.ok){
     Swal.fire(
       "Ëxito",
-      "Usuario creado",
+      userUpdate ? "Usuario Actualizado" : "Usuario creado",
       "success"
     )
   closeModal();
@@ -87,6 +125,31 @@ async function createUser() {
   
 }
 
+//3. Change Status
+async function changeStatus(id, estadoActual){
+
+  const newState= estadoActual === 1 ? 0 : 1;
+  const accion= newState ===1  ? "activar" : "inactivar";
+
+  Swal.fire({
+    title: `¿Seguro que quiere ${accion} este usuario?`,
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonText: "Si, cambiar estado"
+  }).then((result)=>{
+    if(result.isConfirmed){
+      fetch(`${API_BASE}/user/change-status/${id}`, {method: "PATCH"})
+      .then(()=> {
+        Swal.fire("Cambiado", "El estado ha sido actualizado", "success");
+         loadUsers()
+      })
+      .catch(()=>{
+        Swal.fire("Error", "No se pudo cambiar el estado", "error");
+      })
+
+    }
+  })
+}
 //Cargar data
 loadUsers()
 
